cb549c5cdcdfae3a3850564caafd080ddfea9542
mayank gupta <mayankgt123@gmail.com>
2024-02-23 23:40:24 +0530

    -Modified class in Button.jsx -Added flow for displaying all projects in the Sidebar -Added the functions of Cancel button and Add projects flow in the App.jsx file

--- src/components/ProjectSidebar.jsx
+++ src/components/ProjectSidebar.jsx
@@ -1,17 +1,23 @@
 import Button from "./Button.jsx";
 
-export default function ProjectSidebar(){
+export default function ProjectSidebar({onStartAddProject, projects}){
     return <aside className="w-1/3 px-16 py-12 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
         <h2 className="mb-8 font-bold md:text-xl uppercase text-stone-200">
-            Your Project
+            Your Projects
         </h2>
         <div>
-           <Button>
+           <Button onClick={onStartAddProject}>
                 +Add Project
             </Button>
         </div>
-        <ul>
-
+        <ul className="mt-6">
+            {projects.map((project) => (
+                <li key={project.id}>
+                    <button className="w-full text-left px-2 py-1 rounded-sm my-1.5 text-stone-400 hover:text-stone-200 hover:bg-stone-800">
+                        {project.title}
+                    </button>
+                </li>
+            ))}
         </ul>
     </aside>
 }
